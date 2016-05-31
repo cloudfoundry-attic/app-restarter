7a608137bad78f8b9c8b57720de7211f42473d18
Eric Saxby <esaxby@pivotal.io>
2016-05-31 09:54:39 -0700

    allow plugin to be cleanly uninstalled
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- commands/app_restarter_context.go
+++ commands/app_restarter_context.go
@@ -5,7 +5,8 @@ import "github.com/cloudfoundry-incubator/app-restarter/api"
 type AppRestarterContext struct {
 	CLIConnection api.Connection
 
-	RestartApps RestartAppsCommand `command:"restart-apps" description:"Restart all apps"`
+	RestartApps     RestartAppsCommand `command:"restart-apps" description:"Restart all apps"`
+	UninstallPlugin UninstallHook      `command:"CLI-MESSAGE-UNINSTALL"`
 }
 
 var Context AppRestarterContext
